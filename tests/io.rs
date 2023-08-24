ca7d257eb0d679767ed810c32bc46f8689caac9d
Paul Stey <paulstey@gmail.com>
2023-08-24 17:16:11 -0400

    feat: add deid module and a few tests

--- tests/io.rs
+++ tests/io.rs
@@ -1,8 +1,9 @@
 use labyrinth::io;
+use std::path::Path;
 
 #[test]
 fn can_read_csv() {
-    let df = io::read_csv("data/mock_data.csv").expect("Failed to read CSV file");
+    let df = io::read_csv(Path::new("data/mock_data.csv")).expect("Failed to read CSV file");
 
     assert_eq!(df.shape(), (1000, 9));
 }
